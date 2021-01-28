3c95e888d3b003fc09a30a5f67fd49a37a8a0a22
PaulOdon <andrianantenainapaulodon@g.mail.com>
2021-01-28 05:41:43 +0300

    front terminé

---
+++ components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column is-6 is-offset-one-quarter">
+          <br /><br /><br />
+          <h1 class="is-size-4 has-text-centered">Se connecter</h1>
+          <br>
+          <div class="container">
+            <b-field label="Adresse email *">
+              <b-input type="email" maxlength="40"> </b-input>
+            </b-field>
+
+            <b-field label="Mot de passe *">
+              <b-input type="password" password-reveal> </b-input>
+            </b-field>
+
+            <div class="has-text-centered">
+              <br>
+              <b-button type="is-primary" icon-left="check">Valider</b-button>
+              <p><a href="/forgotten-pwd"  class="is-size-7">Mot de passe oublié?</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+  .input, .button {
+    border-radius: 0%;
+  }
+</style>
---
+++ pages/forgotten-pwd.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column is-6 is-offset-one-quarter">
+          <br /><br /><br />
+          <h1 class="is-size-4">Récupération de votre mot de passe</h1>
+          <br>
+          <div class="container">
+            <p>Entrer votre adresse email ci-dessous pour récupérer votre mot de passe.</p>
+
+            <b-field>
+              <b-input type="email" maxlength="40"> </b-input>
+            </b-field>
+
+            <div class="has-text-centered">
+              <b-button type="is-primary">Enoyer</b-button>
+              <br>
+              <p><a href="/"  class="is-size-7">Connexion?</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style>
+  .input, .button {
+    border-radius: 0%;
+  }
+</style>
